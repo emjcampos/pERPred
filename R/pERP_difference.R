b1c95f89316cd77deddebaefc3781a5895ae83c5
Emilie Campos <ecampos@emilies-imac.lan>
2019-07-03 14:52:50 -0700

    added an option for testing "double difference" in which one could compute match - mismatch or correct - incorrect tests. also, now the groups can be a "group of groups" i.e. c("mvASD", "vASD")

--- R/pERP_difference.R
+++ R/pERP_difference.R
@@ -8,14 +8,14 @@
 #' @param group1 A string of the first treatment/diagnosis group to test. If "Overall", all of the individuals will be averaged.
 #' @param task2 A string of the second task for comparison, set to the same value as task1 by default.
 #' @param group2 A string of the second treatment/diagnosis group to test. If "Overall", all of the individuals will be averaged.
+#' @param double_difference A logical to contrast task1 - task2. Use in cases such as correct - incorrect.
 #'
 #' @return A dataframe of t-test results
 #' @export
 #' @importFrom stats pt
 #'
 
-pERP_difference <- function(scores, electrode, task1, group1 = "Overall",
-                            task2 = task1, group2 = group1) {
+pERP_difference <- function(scores, electrode, task1, group1 = "Overall", task2 = task1, group2 = group1, double_difference = FALSE) {
 
   term <- NULL
   Average <- NULL
@@ -27,6 +27,8 @@ pERP_difference <- function(scores, electrode, task1, group1 = "Overall",
   SE2 <- NULL
   n1 <- NULL
   n2 <- NULL
+  rm(list = c("term", "Average", "APSD", "SE", "Average1", "Average2", "SE1",
+              "SE2", "n1", "n2"))
 
   group_scores <- function(task, group, electrode) {
 
@@ -37,10 +39,12 @@ pERP_difference <- function(scores, electrode, task1, group1 = "Overall",
     Average <- NULL
     SE <- NULL
     group_member <- NULL
+    rm(list = c("Task", "Electrode", "term", "estimate", "Average", "SE",
+                "group_member"))
 
-    if (group == "Overall") {
+    if (any(group == "Overall")) {
       scores %>%
-        filter(grepl(task, Task),
+        filter(Task == task,
                Electrode == electrode) %>%
         group_by(Electrode, term) %>%
         summarise(Average = mean(estimate),
@@ -48,47 +52,132 @@ pERP_difference <- function(scores, electrode, task1, group1 = "Overall",
                   SE = sd(estimate)/sqrt(n()),
                   t = Average/SE,
                   n = n()) %>%
-        mutate("Group" = task) %>%
         ungroup()
     } else {
       scores %>%
-        filter(grepl(task, Task),
+        filter(Task == task,
                Electrode == electrode,
-               group_member == group) %>%
+               group_member %in% group) %>%
         group_by(Electrode, term) %>%
         summarise(Average = mean(estimate),
                   APSD = sd(estimate),
                   SE = sd(estimate)/sqrt(n()),
                   t = Average/SE,
                   n = n()) %>%
-        mutate("Group" = task) %>%
         ungroup()
     }
   }
 
-  group_1_all <- group_scores(task1, group1, electrode)
-  group_2_all <- group_scores(task2, group2, electrode)
+  group_scores_double_diff <- function(tasks, group, electrode) {
 
-  cbind(
-    select(group_1_all, term, Average, APSD, SE, t, n),
-    select(group_2_all, Average, APSD, SE, t, n)
-  ) %>%
-    setNames(c("pERP", "Average1", "APSD1", "SE1", "t1", "n1",
-               "Average2", "APSD2", "SE2", "t2", "n2")) %>%
-    mutate("Mean Difference" = Average1 - Average2,
-           "SE Difference" = sqrt(SE1^2 + SE2^2),
-           t = (Average1 - Average2)/sqrt(SE1^2 + SE2^2),
-           significant = ifelse(2*pt(abs(t),
-                                     n1 + n2 - 2,
-                                     lower.tail = FALSE) < 0.054,
-                                "*",
-                                " ")) %>%
-    setNames(c("pERP",
-               paste(task1, group1, "Mean"), paste(task1, group1, "APSD"),
-               paste(task1, group1, "SE"), paste(task1, group1, "t"),
-               paste(task1, group1, "N"),
-               paste(task2, group2, "Mean"), paste(task2, group2, "APSD"),
-               paste(task2, group2, "SE"),
-               paste(task2, group2, "t"), paste(task2, group2, "N"),
-               "Mean Difference", "SE Difference", "t", "signif"))
+    Task <- NULL
+    Subject <- NULL
+    Electrode <- NULL
+    term <- NULL
+    estimate <- NULL
+    Average <- NULL
+    SE <- NULL
+    group_member <- NULL
+    rm(list = c("Task", "Electrode", "term", "estimate", "Average", "SE",
+                "group_member"))
+
+    if (any(group == "Overall")) {
+      scores %>%
+        filter(Task %in% tasks,
+               Electrode == electrode) %>%
+        select(Task, Subject, group_member, Electrode, term, estimate) %>%
+        spread(Task, estimate) %>%
+        mutate(Difference = !!sym(tasks[1]) - !!sym(tasks[2])) %>%
+        gather(Task, estimate,
+               -c(Subject, group_member, Electrode, term)) %>%
+        filter(Task == "Difference") %>%
+        group_by(Electrode, term) %>%
+        summarise(Average = mean(estimate),
+                  APSD = sd(estimate),
+                  SE = sd(estimate)/sqrt(n()),
+                  t = Average/SE,
+                  n = n()) %>%
+        ungroup()
+    } else {
+      scores %>%
+        filter(Task %in% tasks,
+               Electrode == electrode,
+               group_member %in% group) %>%
+        select(Task, Subject, group_member, Electrode, term, estimate) %>%
+        spread(Task, estimate) %>%
+        mutate(Difference = !!sym(tasks[1]) - !!sym(tasks[2])) %>%
+        gather(Task, estimate,
+               -c(Subject, group_member, Electrode, term)) %>%
+        filter(Task == "Difference") %>%
+        group_by(Electrode, term) %>%
+        summarise(Average = mean(estimate),
+                  APSD = sd(estimate),
+                  SE = sd(estimate)/sqrt(n()),
+                  t = Average / SE,
+                  n = n()) %>%
+        ungroup()
+    }
+  }
+
+  if(double_difference) {
+    group_1_all <- group_scores_double_diff(c(task1, task2), group1, electrode)
+    group_2_all <- group_scores_double_diff(c(task1, task2), group2, electrode)
+
+    cbind(
+      select(group_1_all, term, Average, APSD, SE, t, n),
+      select(group_2_all, Average, APSD, SE, t, n)
+    ) %>%
+      setNames(c("pERP", "Average1", "APSD1", "SE1", "t1", "n1",
+                 "Average2", "APSD2", "SE2", "t2", "n2")) %>%
+      mutate("Mean Difference" = Average1 - Average2,
+             "SE Difference" = sqrt(SE1^2 + SE2^2),
+             t = (Average1 - Average2)/sqrt(SE1^2 + SE2^2),
+             significant = ifelse(2*pt(abs(t),
+                                       n1 + n2 - 2,
+                                       lower.tail = FALSE) < 0.054,
+                                  "*",
+                                  " ")) %>%
+      setNames(c("pERP",
+                 paste(task1, "-", task2, paste(group1, collapse = ', '), "Mean"),
+                 paste(task1, "-", task2, paste(group1, collapse = ', '), "APSD"),
+                 paste(task1, "-", task2, paste(group1, collapse = ', '), "SE"),
+                 paste(task1, "-", task2, paste(group1, collapse = ', '), "t"),
+                 paste(task1, "-", task2, paste(group1, collapse = ', '), "N"),
+                 paste(task1, "-", task2, paste(group2, collapse = ', '), "Mean"),
+                 paste(task1, "-", task2, paste(group2, collapse = ', '), "APSD"),
+                 paste(task1, "-", task2, paste(group2, collapse = ', '), "SE"),
+                 paste(task1, "-", task2, paste(group2, collapse = ', '), "t"),
+                 paste(task1, "-", task2, paste(group2, collapse = ', '), "N"),
+                 "Mean Difference", "SE Difference", "t", "signif"))
+  } else {
+    group_1_all <- group_scores(task1, group1, electrode)
+    group_2_all <- group_scores(task2, group2, electrode)
+
+    cbind(
+      select(group_1_all, term, Average, APSD, SE, t, n),
+      select(group_2_all, Average, APSD, SE, t, n)
+    ) %>%
+      setNames(c("pERP", "Average1", "APSD1", "SE1", "t1", "n1",
+                 "Average2", "APSD2", "SE2", "t2", "n2")) %>%
+      mutate("Mean Difference" = Average1 - Average2,
+             "SE Difference" = sqrt(SE1^2 + SE2^2),
+             t = (Average1 - Average2)/sqrt(SE1^2 + SE2^2),
+             significant = ifelse(2*pt(abs(t),
+                                       n1 + n2 - 2,
+                                       lower.tail = FALSE) < 0.054,
+                                  "*",
+                                  " ")) %>%
+      setNames(c("pERP",
+                 paste(task1, paste(group1, collapse = ', '), "Mean"),
+                 paste(task1, paste(group1, collapse = ', '), "APSD"),
+                 paste(task1, paste(group1, collapse = ', '), "SE"),
+                 paste(task1, paste(group1, collapse = ', '), "t"),
+                 paste(task1, paste(group1, collapse = ', '), "N"),
+                 paste(task2, paste(group2, collapse = ', '), "Mean"),
+                 paste(task2, paste(group2, collapse = ', '), "APSD"),
+                 paste(task2, paste(group2, collapse = ', '), "SE"),
+                 paste(task2, paste(group2, collapse = ', '), "t"),
+                 paste(task2, paste(group2, collapse = ', '), "N"),
+                 "Mean Difference", "SE Difference", "t", "signif"))
+  }
 }
